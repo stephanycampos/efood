9cd5adcb6347c997a4375b13db4dfcea3e40267b
stephanycampos <stephanyenne@gmail.com>
2026-01-15 23:07:51 -0300

    Atualizando link API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,30 +1,31 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi } from '@reduxjs/toolkit/query/react'
+import { fetchBaseQuery } from '@reduxjs/toolkit/query'
 
 const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'https://ebac-fake-api.vercel.app/api/efood'
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/'
+  }),
+  endpoints: (builder) => ({
+    getOnRestaurants: builder.query<Restaurants[], void>({
+      query: () => 'restaurantes'
     }),
-    endpoints: (builder) => ({
-        getOnRestaurants: builder.query<Restaurants[], void>({
-            query: () => 'restaurantes'
-        }),
-        getRestaurantMenu: builder.query<Restaurants, number>({
-            query: (id) => `restaurantes/${id}`
-        }),
-        purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
-            query: (body) => ({
-                url: 'checkout',
-                method: 'POST',
-                body
-            })
-        })
+    getRestaurantMenu: builder.query<Restaurants, number>({
+      query: (id) => `restaurantes/${id}`
+    }),
+    purchase: builder.mutation<PurchaseResponse, PurchasePayload>({
+      query: (body) => ({
+        url: 'checkout',
+        method: 'POST',
+        body
+      })
     })
+  })
 })
 
 export const {
-    useGetOnRestaurantsQuery,
-    useGetRestaurantMenuQuery,
-    usePurchaseMutation
+  useGetOnRestaurantsQuery,
+  useGetRestaurantMenuQuery,
+  usePurchaseMutation
 } = api
 
 export default api
